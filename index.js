3fa3c0519bfac022b092449ccff5608fb6ef32bd
khushveer <khushveermalviya26@gmail.com>
2024-12-03 22:47:18 +0530

    update the packages

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-  import dotenv from 'dotenv';
-  import express from "express";
-    import cors from "cors";
-  import { graphqlHTTP } from 'express-graphql';
-  import schema from "./Schema/Schema.js";
-import  AzureDb from './db/SecondDb.js';
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import { graphqlHTTP } from 'express-graphql';
+import schema from './Schema/Schema.js';
+import AzureDb from './db/SecondDb.js';
 
-  dotenv.config();
+dotenv.config();
 
-  const app = express();
-  app.use(cors());
-  app.use(express.json());
-  AzureDb();
-  app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql: true
-  }));
+const app = express();
 
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+// Enable CORS for all routes
+app.use(cors({
+  origin: 'http://localhost:5173', // Replace with your frontend's origin
+  credentials: true
+}));
+
+app.use(express.json());
+AzureDb();
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
