8fde5280af339acb62171cdf789fabede6f6c4ae
khushveer <khushveermalviya26@gmail.com>
2024-12-04 11:23:22 +0530

    d

--- Schema/Schema.js
+++ Schema/Schema.js
@@ -1,111 +1,83 @@
-import { GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLNonNull, GraphQLList, GraphQLInt } from 'graphql';
-import jwt from "jsonwebtoken"
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLNonNull, GraphQLList } from 'graphql';
+import jwt from "jsonwebtoken";
 import sql from 'mssql';
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+
 dotenv.config();
-const SECRET_KEY = 
-"asdfhasdfjkah";
-// Student Login 
+
+const SECRET_KEY = "asdfhasdfjkah";
+
+// Define StudentLogin GraphQL Object Type
 const StudentLoginType = new GraphQLObjectType({
     name: 'StudentLogin',
     fields: () => ({
         StudentID: { type: GraphQLString },
         FirstName: { type: GraphQLString },
-        LastName: { type: GraphQLString }
-    })
+        LastName: { type: GraphQLString },
+    }),
 });
 
-const FaultyloginType = new GraphQLObjectType({
-    name:"FacultyLogin",
-    fields : ()=>({
-       Username:{ type: GraphQLString },
-       token: { type: GraphQLString },
-      School_Id :{type:GraphQLString}
-    })
-})
+// Define FaultyLogin GraphQL Object Type
+const FaultyLoginType = new GraphQLObjectType({
+    name: "FacultyLogin",
+    fields: () => ({
+        Username: { type: GraphQLString },
+        token: { type: GraphQLString },
+        School_Id: { type: GraphQLString },
+    }),
+});
 
-
-// Root Query
+// Define RootQuery
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
-    
-       
-        
         studentLogin: {
             type: StudentLoginType,
             args: {
                 Username: { type: new GraphQLNonNull(GraphQLString) },
-                Password: { type: new GraphQLNonNull(GraphQLString) }
+                Password: { type: new GraphQLNonNull(GraphQLString) },
             },
             resolve(parent, args) {
                 return sql.query`SELECT StudentID, FirstName, LastName FROM Students WHERE StudentID = ${args.Username} AND Password = ${args.Password}`
                     .then(result => result.recordset[0]);
-            }
-
-
-        },
-        Facultylogin :{
-            type:FaultyloginType,
-            args :
-            {
-                Username: {type: new GraphQLNonNull(GraphQLString)},
-                Password: {type: new GraphQLNonNull(GraphQLString)}
             },
-            resolve(parent,args){
-                return sql.query`SELECT Username,Password  FROM Faculty WHERE Username = ${args.Username} AND Password =${args.Password} `
-                .then(async result =>{
-                    const faculty= result.recordset[0];
-                    if(!faculty){
-                        throw new Error("invaild credentials")
-                    }
-                    const token = jwt.sign({ username: faculty.Username }, SECRET_KEY, { expiresIn: '1h' });
-                    console.log("Generated Token:", token);
-                    return {
-                        token,
-                        username: faculty.Username,
-                    };
-                    
-                })
-            }
-        }
-    }
+        },
+        facultyLogin: {
+            type: FaultyLoginType,
+            args: {
+                Username: { type: new GraphQLNonNull(GraphQLString) },
+                Password: { type: new GraphQLNonNull(GraphQLString) },
+            },
+            resolve(parent, args) {
+                return sql.query`SELECT Username, Password FROM Faculty WHERE Username = ${args.Username} AND Password = ${args.Password}`
+                    .then(async result => {
+                        const faculty = result.recordset[0];
+                        if (!faculty) {
+                            throw new Error("Invalid credentials");
+                        }
+                        const token = jwt.sign({ username: faculty.Username }, SECRET_KEY, { expiresIn: '1h' });
+                        console.log("Generated Token:", token);
+                        return {
+                            token,
+                            Username: faculty.Username,
+                        };
+                    });
+            },
+        },
+    },
 });
 
-// Mutation     
-// const Mutation = new GraphQLObjectType({
-//     name: 'Mutation',
-//     fields: {
-//         addFaculty: {
-//             type: FacultyType,
-//             args: {
-//                 Username: { type: new GraphQLNonNull(GraphQLString) },
-//                 Password: { type: new GraphQLNonNull(GraphQLString) },
-//                 Location: { type: new GraphQLNonNull(GraphQLString) },
-//                 SchoolName: { type: new GraphQLNonNull(GraphQLString) }
-//             },
-//             resolve(parent, args) {
-//                 return sql.query`INSERT INTO Faculty (Username, Password, Location, SchoolName) VALUES (${args.Username}, ${args.Password}, ${args.Location}, ${args.SchoolName})`
-//                     .then(() => ({ Username: args.Username, Password: args.Password, Location: args.Location, SchoolName: args.SchoolName }));
-//             }
-//         },
-//         Edit:{
-//             type:FacultyType,
-//             args:{
-//                 Username: { type: new GraphQLNonNull(GraphQLString) },
-//                 Password: { type: new GraphQLNonNull(GraphQLString) }
-//             },
-//             resolve(parent,args){
-//                 return sql.query`UPDATE Faculty SET Username = ${args.Username}, Password = ${args.Password} WHERE Username = ${args.Username}`
-//                     .then(() => ({ Username: args.Username, Password: args.Password }));
-//             }
-//         }
-//     }
-// });
-
+// Create GraphQL Schema
 const schema = new GraphQLSchema({
     query: RootQuery,
-    // mutation: Mutation
 });
 
+// Set Up Apollo Server
+const server = new ApolloServer({
+    schema,
+});
+
+// Start Server
 export default schema;
--- index.js
+++ index.js
@@ -1,29 +1,53 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
-import { graphqlHTTP } from 'express-graphql';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import bodyParser from 'body-parser';
 import schema from './Schema/Schema.js';
 import AzureDb from './db/SecondDb.js';
 
 dotenv.config();
 
+// Initialize the Express app
 const app = express();
 
-// Enable CORS for all routes
+// Enable CORS
 app.use(cors({
   origin: 'http://localhost:5173', // Replace with your frontend's origin
-  credentials: true
+  credentials: true,
 }));
 
-app.use(express.json());
+// Parse JSON request body
+app.use(bodyParser.json());
+
+// Connect to Azure DB
 AzureDb();
 
-app.use('/graphql', graphqlHTTP({
+// Initialize Apollo Server
+const server = new ApolloServer({
   schema,
-  graphiql: true
-}));
+  introspection: true, // Enable introspection for GraphiQL or Apollo Sandbox
+});
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// Start the Apollo Server and integrate with Express
+const startServer = async () => {
+  await server.start();
+  app.use('/graphql', expressMiddleware(server, {
+    context: async ({ req }) => {
+      // You can add context logic here, e.g., extracting a user from the token
+      const token = req.headers.authorization || '';
+      return { token };
+    },
+  }));
+
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    console.log(`GraphQL endpoint: http://localhost:${PORT}/graphql`);
+  });
+};
+
+startServer().catch(err => {
+  console.error('Error starting server:', err);
 });
